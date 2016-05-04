699d473b4b8825a1856fdbae79d20d4528651af1
sicwere <sicwere@users.noreply.github.com>
2016-05-04 16:17:36 -0400

    Modified so that OSO image appears in carousel

--- layout/about.php
+++ layout/about.php
@@ -1,11 +1,7 @@
 <?php
 /**
  * @file about.php
- * Displays the about section
- * Also displays the new search box
- *
- *
- 
+ * Displays the about section.
  */
 
 // Replicate an image carousel database.
@@ -36,9 +32,14 @@ $carousel = array(
 		"alt" => "South Carolina and the Civil War"
 	),
 	array(
+		"image" => "oldsouthernorchards.png",
+		"alt" => "Old Southern Orchards"
+	),
+	// Southern Periodical Humor Repository may be added back at a later time
+	/*array(
 		"image" => "sphr.png",
 		"alt" => "Southern Periodical Humor Repository"
-	),
+	),*/
 	array(
 		"image" => "ravenel.png",
 		"alt" => "Plants and Planters - Henry William Ravenel"
@@ -48,7 +49,7 @@ $carousel = array(
 <section class="container-fluid" id="about">
 	<div class="row">
 
-		<div class="col-md-5 center-block" id="carousel">
+		<div class="col-md-8 center-block" id="carousel">
 			<?php
 			foreach ($carousel as $image) {
 				if (isset($image["image"]) && file_exists("img/carousel/" . $image["image"]) && $image["image"] != "") {
@@ -65,29 +66,6 @@ $carousel = array(
 	<div class="row text-justify">
 		<div class="col-md-10 center-block">
 			<p>The goal of the Digital US South Initiative is to combine rigorous scholarship and new technologies in ways that will promote and enhance thoughtful engagement with the South’s history, cultures, and landscapes. These projects are designed with both scholar and non-scholar in mind and provide opportunities for classroom teaching, new scholarly production, and individual study and enjoyment. Currently, DUSS federates twelve projects, with topics as diverse as agriculture, 19th-century literature, natural history, the Civil War, and the Civil Rights Movement. These projects have benefited from the generous financial support of the University of South Carolina’s College of Arts and Sciences, University Libraries, USC's Center for Digital Humanities, the Watson-Brown Foundation, and the Humanities Council<sup>SC</sup>.</p>
-		</div>
-	</div>
-	<div class="row text-center">
-		<div class="col-md-9 center-block">
-			<form class="form-horizontal" id="home-search" name="home-search" method="GET" action="search.php">
-			<div class="row">
-				<input type="text" name="query" id="query" class="form-control" placeholder="Search our projects"><br>
-			</div>
-			<div class="row">
-				<div class="col-xs-4">
-					<input type="checkbox" value="false" name="full-text-search" id="full-text-search">
-					<label for="full-text-search" class="control-label">Search full text</label><br>
-					<input type="checkbox" value="false" name="new-tab" id="new-tab">
-					<label for="new-tab" class="control-label">Open results in new tab</label>
-				</div>
-				<div class="col-xs-4">
-					<input type="submit" class="btn btn-primary" name="Search" value="Search">
-				</div>
-				<div class="col-xs-4">
-					<a class="form-control col-xs-5 center-block" href="search.php">Advanced search</a>
-				</div>
-			</div>
-			</form>
 		</div>
 	</div>
 
