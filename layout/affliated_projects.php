52a1ee6ce386f976521b9adfae23280178bf2dcb
sicwere <sicwere@users.noreply.github.com>
2016-05-11 16:44:23 -0400

    New files

---
+++ layout/affliated_projects.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file affliated_projects.php
+ * 
+ */
+?><!DOCTYPE html>
+<head>
+<title>Digital United States South</title>
+</head>
+<body>
+Affliated Projects
+</body>
+</html>
---
+++ layout/core_projects.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file core_projects.php
+ * 
+ */
+?><!DOCTYPE html>
+<head>
+<title>Digital United States South</title>
+</head>
+<body>
+Core Projects
+</body>
+</html>
